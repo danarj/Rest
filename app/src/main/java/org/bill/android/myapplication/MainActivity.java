faca4db827ec4badfb18eea7d2f12970a3e6ae92
danarj <danarj@live.com>
2016-06-28 00:23:29 +0300

    Main activity modifications

--- app/src/main/java/org/bill/android/myapplication/MainActivity.java
+++ app/src/main/java/org/bill/android/myapplication/MainActivity.java
@@ -4,6 +4,9 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import org.bill.android.myapplication.membershipService.RIVAuthenticateUserResult;
@@ -14,33 +17,45 @@ import org.bill.android.myapplication.membershipService.RIVResultHeader;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView finalResult;
+    private TextView txtViewResult;
+    private EditText editTextUserName;
+    private EditText editTextPassword;
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        finalResult = (TextView) findViewById(R.id.viewMain);
-        AsyncTaskRunner asyncTaskRunner = new AsyncTaskRunner();
-        asyncTaskRunner.execute();
+        txtViewResult = (TextView) findViewById(R.id.txtViewResult);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        editTextUserName = (EditText) findViewById(R.id.editTextUserName);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+
+            @Override public void onClick(View v) {
+                new Login().execute(editTextUserName.getText().toString(), editTextPassword.getText().toString());
+            }
+        });
+//        AsyncTaskRunner asyncTaskRunner = new AsyncTaskRunner();
+//        asyncTaskRunner.execute();
     }
 
-    private class AsyncTaskRunner extends AsyncTask<String, String, String> {
+    private class Login extends AsyncTask<String, String, String> {
         private String resp;
 
         @Override
         protected String doInBackground(String... params) {
-            publishProgress("Sleeping..."); // Calls onProgressUpdate()
+            // publishProgress("Traying to login..."); // Calls onProgressUpdate()
             try {
                 RIVRequestHeader request = new RIVRequestHeader();
-                request.Username = "danar";
-                request.Password = "12345";
+                request.Username = params[0];
+                request.Password = params[1];
                 try {
                     RIVBasicHttpBinding_IMembershipService membershipService = new RIVBasicHttpBinding_IMembershipService();
                     RIVAuthenticateUserResult rivAuthenticateUserResult = membershipService.AuthenticateUser(request);
                     RIVResultHeader resultHeader = rivAuthenticateUserResult.ResultHeader;
                     if (resultHeader.IsSuccess) {
-                        resp="Success";
+                        resp = "Success";
                     }
                     Log.e("error", resultHeader.ResultMessage);
                 } catch (Exception e) {
@@ -56,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String result) {
             // execution of result of Long time consuming operation
-            finalResult.setText(resp);
+            txtViewResult.setText(resp);
         }
 
         @Override
@@ -67,7 +82,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void onProgressUpdate(String... text) {
-            finalResult.setText(text[0]);
+            txtViewResult.setText(text[0]);
             // Things to be done while execution of long running operation is in
             // progress. For example updating ProgessDialog
         }
